51f41507fa78fd96dff4d98a84a03ee774e660f1
wiktorlesiak <B00087701@student.itb.ie>
2019-02-13 13:57:14 +0000

    may be working on phone

--- app/src/main/java/com/trip/planner/HomeScreen.java
+++ app/src/main/java/com/trip/planner/HomeScreen.java
@@ -1,22 +1,27 @@
 package com.trip.planner;
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeScreen extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-
+        startButton = (Button) findViewById(R.id.startButton);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
@@ -24,6 +29,14 @@ public class HomeScreen extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this, MapsActivity.class));
+            }
+        });
     }
 
 
